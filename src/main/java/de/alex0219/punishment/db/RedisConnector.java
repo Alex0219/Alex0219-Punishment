18b8a7f3b27d6044ed7c1292e65f87951af1b32b
Alexander Fiedler <javainputstream@gmail.com>
2020-06-11 02:22:33 +0200

    Fixed potential redis pool memory leak

--- src/main/java/de/alex0219/punishment/db/RedisConnector.java
+++ src/main/java/de/alex0219/punishment/db/RedisConnector.java
@@ -46,6 +46,8 @@ public class RedisConnector {
     public Jedis getJedis() {
         try (Jedis jedis = jedisPool.getResource()) {
             return jedis;
+        } finally {
+            jedisPool.close();
         }
     }
 
